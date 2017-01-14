4efa0f505ef5942429ffe0929b1e4bf172e57ff2
Superwoods <st_sister@iCloud.com>
2017-01-14 10:54:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  * @Email:  st_sister@iCloud.com
  * @Filename: index.js
 * @Last modified by:   SuperWoods
-* @Last modified time: 2017-01-14-03:22:01
+* @Last modified time: 2017-01-14-10:51:32
  * @License: MIT
  * @Copyright: Copyright (c) Xinhuanet Inc. All rights reserved.
  */
@@ -604,44 +604,78 @@ $(() => {
             // autoplay: 12000,
             // parallax: true,
             // pagination: $scenes3In.selector + ' .swiper-pagination',
-            prevButton: '#scenes3-in-prev' + num,
-            nextButton: '#scenes3-in-next' + num,
+            // prevButton: '#scenes3-in-prev' + num,
+            // nextButton: '#scenes3-in-next' + num,
             paginationClickable: true,
             // speed: 3000,
             slidesPerView: 5,
             slidesPerGroup: 5,
             spaceBetween: 30,
             // runCallbacksOnInit: true,
-            nested: true,
+            nested: true, // 不能使用loop: true
             // resistanceRatio: 0,
             onInit: function(swiper) {
                 (num === 0) && scenes3BtnShow(swiper.slides.eq(0));
-                swiper.nextButton.css({
-                    'opacity': 1,
-                    'pointer-events': 'visible',
-                    'cursor': 'pointer',
-                })
+                const $nextBtn = $('#scenes3-in-next' + num);
+                const $nextPrev = $('#scenes3-in-prev' + num);
+
+                // $nextBtn.css({
+                //     'opacity': 1,
+                //     'pointer-events': 'visible',
+                //     'cursor': 'pointer',
+                // })
 
                 console.log(num, scenes3SwiperIns[num + 1] === undefined);
-
-                // swiper.nextButton.off('click');
-                swiper.nextButton.on('click', function() {
+                // nextBtn
+                $nextBtn.off('click');
+                $nextBtn.on('click', function() {
+                    swiper.slideNext();
                     if (swiper.isEnd) {
                         $(this).off('click');
-                        $(this).click(function(){
+                        $(this).click(function() {
                             if (scenes3SwiperIns[num + 1] === undefined) {
                                 scenes3SwiperIns[num + 1] = scenes3SwiperInInit(num + 1);
-                            } else {
-
                             }
                             scenes3Swiper.slideNext();
-                            $(this).off('click');
+                            // $(this).off('click');
                         });
-                    } else {
 
+                        /*
+                            2017-01-14-10.34 优化备忘 @St.
+                            缺少判断什么时候让按钮变为不能点击状态，
+                            因为嵌套原因逻辑存在一些问题需要梳理
+                        */
+                        // if (scenes3Swiper.isEnd) {
+                        //     $(this).css({
+                        //         'opacity': 0.5,
+                        //         'pointer-events': 'none',
+                        //         // 'cursor': 'pointer',
+                        //     })
+                        // } else {
+                        //     $(this).css({
+                        //         'opacity': 1,
+                        //         'pointer-events': 'visible',
+                        //         'cursor': 'pointer',
+                        //     })
+                        // }
                     }
                 });
 
+                // prevBtn
+                $nextPrev.off('click');
+                $nextPrev.on('click', function() {
+                    swiper.slidePrev();
+                    if (swiper.isBeginning) {
+                        $(this).off('click');
+                        $(this).click(function() {
+                            // if (scenes3SwiperIns[num + 1] === undefined) {
+                            //     scenes3SwiperIns[num + 1] = scenes3SwiperInInit(num + 1);
+                            // }
+                            scenes3Swiper.slidePrev();
+                            // $(this).off('click');
+                        });
+                    }
+                });
             },
         });
     };
@@ -765,14 +799,227 @@ $(() => {
                 }
             });
         }
-    };;
+    };
+    const triangleBgHandler = function() {
+        console.log('is cav');
+        var $triangleBg = $("#triangleBg");
+        var $triangleBgAnitOut = $("#triangleBg-anitOut");
+        if (!!document.createElement("canvas").getContext) {
+            // $.getScript("/lib/triangleBg/cav.min.js", function(){})
+            // function() {
+            var t = {
+                width: 0.8,
+                height: 1,
+                depth: 40,
+                segments: 5,
+                slices: 0,
+                xRange: 0.5,
+                yRange: 0.2,
+                zRange: 1,
+                ambient: "#444444",
+                diffuse: "#FFFFFF",
+                speed: 0.0003
+            };
+            var G = {
+                count: 2,
+                xyScalar: 1,
+                zOffset: 100,
+                ambient: "#FFFFFF",
+                diffuse: "#FFFFFF",
+                speed: 0.001,
+                gravity: 1200,
+                dampening: 0.95,
+                minLimit: 10,
+                maxLimit: 1,
+                minDistance: 20,
+                maxDistance: 400,
+                autopilot: false,
+                draw: false,
+                bounds: CAV.Vector3.create(),
+                step: CAV.Vector3.create(Math.randomInRange(0.2, 1), Math.randomInRange(0.2, 1), Math.randomInRange(0.2, 1))
+            };
+            var m = "canvas";
+            var E = "svg";
+            var x = {
+                renderer: m
+            };
+            var i, n = Date.now();
+            var L = CAV.Vector3.create();
+            var k = CAV.Vector3.create();
+            var z = $triangleBg[0];
+            var w = $triangleBgAnitOut[0];
+            var D, I, h, q, y;
+            var g;
+            var r;
+
+            function C() {
+                F();
+                p();
+                s();
+                B();
+                v();
+                K(z.offsetWidth, z.offsetHeight);
+                o()
+            }
+
+            function F() {
+                g = new CAV.CanvasRenderer();
+                H(x.renderer)
+            }
+
+            function H(N) {
+                if (D) {
+                    w.removeChild(D.element)
+                }
+                switch (N) {
+                    case m:
+                        D = g;
+                        break
+                }
+                D.setSize(z.offsetWidth, z.offsetHeight);
+                w.appendChild(D.element)
+            }
+
+            function p() {
+                I = new CAV.Scene()
+            }
+
+            function s() {
+                I.remove(h);
+                D.clear();
+                q = new CAV.Plane(t.width * D.width, t.height * D.height, t.segments, t.slices);
+                y = new CAV.Material(t.ambient, t.diffuse);
+                h = new CAV.Mesh(q, y);
+                I.add(h);
+                var N, O;
+                for (N = q.vertices.length - 1; N >= 0; N--) {
+                    O = q.vertices[N];
+                    O.anchor = CAV.Vector3.clone(O.position);
+                    O.step = CAV.Vector3.create(Math.randomInRange(0.2, 1), Math.randomInRange(0.2, 1), Math.randomInRange(0.2, 1));
+                    O.time = Math.randomInRange(0, Math.PIM2)
+                }
+            }
+
+            function B() {
+                var O, N;
+                for (O = I.lights.length - 1; O >= 0; O--) {
+                    N = I.lights[O];
+                    I.remove(N)
+                }
+                D.clear();
+                for (O = 0; O < G.count; O++) {
+                    N = new CAV.Light(G.ambient, G.diffuse);
+                    N.ambientHex = N.ambient.format();
+                    N.diffuseHex = N.diffuse.format();
+                    I.add(N);
+                    N.mass = Math.randomInRange(0.5, 1);
+                    N.velocity = CAV.Vector3.create();
+                    N.acceleration = CAV.Vector3.create();
+                    N.force = CAV.Vector3.create()
+                }
+            }
+
+            function K(O, N) {
+                D.setSize(O, N);
+                CAV.Vector3.set(L, D.halfWidth, D.halfHeight);
+                s()
+            }
+
+            function o() {
+                i = Date.now() - n;
+                u();
+                M();
+                requestAnimationFrame(o)
+            }
+
+            function u() {
+                var Q, P, O, R, T, V, U, S = t.depth / 2;
+                CAV.Vector3.copy(G.bounds, L);
+                CAV.Vector3.multiplyScalar(G.bounds, G.xyScalar);
+                CAV.Vector3.setZ(k, G.zOffset);
+                for (R = I.lights.length - 1; R >= 0; R--) {
+                    T = I.lights[R];
+                    CAV.Vector3.setZ(T.position, G.zOffset);
+                    var N = Math.clamp(CAV.Vector3.distanceSquared(T.position, k), G.minDistance, G.maxDistance);
+                    var W = G.gravity * T.mass / N;
+                    CAV.Vector3.subtractVectors(T.force, k, T.position);
+                    CAV.Vector3.normalise(T.force);
+                    CAV.Vector3.multiplyScalar(T.force, W);
+                    CAV.Vector3.set(T.acceleration);
+                    CAV.Vector3.add(T.acceleration, T.force);
+                    CAV.Vector3.add(T.velocity, T.acceleration);
+                    CAV.Vector3.multiplyScalar(T.velocity, G.dampening);
+                    CAV.Vector3.limit(T.velocity, G.minLimit, G.maxLimit);
+                    CAV.Vector3.add(T.position, T.velocity)
+                }
+                for (V = q.vertices.length - 1; V >= 0; V--) {
+                    U = q.vertices[V];
+                    Q = Math.sin(U.time + U.step[0] * i * t.speed);
+                    P = Math.cos(U.time + U.step[1] * i * t.speed);
+                    O = Math.sin(U.time + U.step[2] * i * t.speed);
+                    CAV.Vector3.set(U.position, t.xRange * q.segmentWidth * Q, t.yRange * q.sliceHeight * P, t.zRange * S * O - S);
+                    CAV.Vector3.add(U.position, U.anchor)
+                }
+                q.dirty = true
+            }
+
+            function M() {
+                D.render(I)
+            }
+
+            function J(O) {
+                var Q, N, S = O;
+                var P = function(T) {
+                    for (Q = 0, l = I.lights.length; Q < l; Q++) {
+                        N = I.lights[Q];
+                        N.ambient.set(T);
+                        N.ambientHex = N.ambient.format()
+                    }
+                };
+                var R = function(T) {
+                    for (Q = 0, l = I.lights.length; Q < l; Q++) {
+                        N = I.lights[Q];
+                        N.diffuse.set(T);
+                        N.diffuseHex = N.diffuse.format()
+                    }
+                };
+                return {
+                    set: function() {
+                        P(S[0]);
+                        R(S[1])
+                    }
+                }
+            }
+
+            function v() {
+                window.addEventListener("resize", j)
+            }
+
+            function A(N) {
+                CAV.Vector3.set(k, N.x, D.height - N.y);
+                CAV.Vector3.subtract(k, L)
+            }
+
+            function j(N) {
+                K(z.offsetWidth, z.offsetHeight);
+                M()
+            }
+            C();
+            // }
+        } else {
+            $triangleBg.remove();
+        }
+    };
+
     const triangleBgInit = function(swiperRealIndex, num) {
         if (swiperRealIndex === num && triangleBg === null) {
             $triangleBg = $('#triangleBg');
-            triangleBg = $.getScript("lib/triangleBg/triangleBg.js", function() {
-                triangleBgShow();
-            });
-            triangleBgHide();
+            // triangleBg = $.getScript("lib/triangleBg/triangleBg.js", function() {
+            //     triangleBgShow();
+            // });
+            triangleBg = triangleBgHandler();
+            triangleBgShow();
+            // triangleBgHide();
         }
     };
 
